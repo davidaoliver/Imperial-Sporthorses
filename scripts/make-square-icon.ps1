7b4c5e8bd4961565b974e9b67e676c9f2a3d4e98
davidaoliver <davidoliver820@gmail.com>
2026-03-03 15:14:58 -0600

    Add square PWA icons (192x192, 512x512) for crisp home screen display

---
+++ scripts/make-square-icon.ps1
@@ -0,0 +1,35 @@
+Add-Type -AssemblyName System.Drawing
+
+$srcPath = Join-Path $PSScriptRoot '..\public\logo.png'
+$out512 = Join-Path $PSScriptRoot '..\public\icon-512.png'
+$out192 = Join-Path $PSScriptRoot '..\public\icon-192.png'
+
+$src = [System.Drawing.Image]::FromFile((Resolve-Path $srcPath).Path)
+Write-Host "Source: $($src.Width) x $($src.Height)"
+
+foreach ($size in @(512, 192)) {
+    $bmp = New-Object System.Drawing.Bitmap($size, $size)
+    $g = [System.Drawing.Graphics]::FromImage($bmp)
+    $g.Clear([System.Drawing.Color]::Black)
+    $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
+    $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::HighQuality
+    $g.PixelOffsetMode = [System.Drawing.Drawing2D.PixelOffsetMode]::HighQuality
+
+    # Fit image inside the square, centered
+    $ratio = [Math]::Min($size / $src.Width, $size / $src.Height)
+    $newW = [int]($src.Width * $ratio)
+    $newH = [int]($src.Height * $ratio)
+    $x = [int](($size - $newW) / 2)
+    $y = [int](($size - $newH) / 2)
+
+    $g.DrawImage($src, $x, $y, $newW, $newH)
+    $g.Dispose()
+
+    $outPath = if ($size -eq 512) { $out512 } else { $out192 }
+    $bmp.Save($outPath, [System.Drawing.Imaging.ImageFormat]::Png)
+    $bmp.Dispose()
+    Write-Host "Created $outPath ($size x $size)"
+}
+
+$src.Dispose()
+Write-Host "Done!"
